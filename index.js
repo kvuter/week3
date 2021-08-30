ed86a6b44a2d287004c5a7d32bde1e755b23faad
kvuter <samsarahmaykim@hotmail.com>
2021-08-30 21:47:05 +1000

    week3

---
+++ index.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#main',
+    data: {
+      message: 'Welcome to I WANT FLOWERS where the freshness of our flowers fills you with happiness and serenity. We aim to deliver with brightening your day, so go ahead and express your love, we can do all through the message of such beauty a flower can bring. Have a look below where you can search an arrange of beautiful flowers.'
+    }
+  })
+
+  new Vue({
+    el: '#checkbox',
+    data: {
+      checkedNames: ["You chose: "]
+    }
+  })
